0320c1849de023548bad86d863361ea99884ff06
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-09-11 13:47:08 -0400

    Create Pythagorean.java

---
+++ Homework/Pythagorean.java
@@ -0,0 +1,26 @@
+/**
+ * Name: Jensen Sung
+ * Description: This program gets an integer from the user and finds all pythagorean triples whos largest number is below the integer
+ * Date: 09/09/2022
+ */
+
+import java.util.Scanner;
+
+public class Pythagorean {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter a positive integer: ");
+		int limit = in.nextInt();
+		
+		for(int c = 0; c <= limit; c++) {
+			for(int b = 0; b < c; b++) {
+				for(int a = 0; a < b; a++) {
+					if(a*a + b*b == c*c) {
+						System.out.println(a + " " + b + " " + c);
+					}
+				}
+			}
+		}
+	}
+}
