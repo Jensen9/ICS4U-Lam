c6de5b3179e61d5cf77976c779a26bac86bb1114
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-09-11 13:49:06 -0400

    Create SortThreeLetters.java

---
+++ Homework/SortThreeLetters.java
@@ -0,0 +1,30 @@
+/**
+ * Name: Jensen Sung
+ * Date: 09/08/2022
+ * Description: This program gets three lowercase letter inputs from the user and sorts them in alphabetical order
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortThreeLetters {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter first character: ");
+		char firstCharacter = in.next().charAt(0);
+		System.out.print("Enter second character: ");
+		char secondCharacter = in.next().charAt(0);
+		System.out.print("Enter third character: ");
+		char thirdCharacter = in.next().charAt(0);
+
+		if(firstCharacter >= 'a' && firstCharacter <= 'z' && secondCharacter >= 'a' && secondCharacter <= 'z' && thirdCharacter >= 'a' && thirdCharacter <= 'z') {
+			char[] allCharacters = {firstCharacter, secondCharacter, thirdCharacter};
+			Arrays.sort(allCharacters);
+			for(char ch: allCharacters) {
+				System.out.print(ch);
+			}
+		}
+		else System.out.println("Enter lowercase letters.");
+	}
+}
