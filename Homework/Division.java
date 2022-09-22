4efabc51d7b80e207fe5ab5201d18257cbdfa7b4
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-09-22 18:56:47 -0400

    Create Division.java

---
+++ Homework/Division.java
@@ -0,0 +1,43 @@
+/**
+ * Name: Jensen Sung
+ * Date: 09/21/2022
+ * Description: Gets user input for numerator and denominator and finds quotient. If denominator is 0 or wrong input type, it lets user know
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Division {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int numerator = 0, denominator = 0;
+		boolean quit = false;
+		while(!quit) {
+			try {
+				System.out.print("Enter the numerator: ");
+				numerator = in.nextInt();
+				System.out.print("Enter the denominator: ");
+				denominator = in.nextInt();
+
+				System.out.println(numerator + " / " + denominator + " is " + (numerator/denominator));
+				quit = true;
+			}
+			catch(ArithmeticException ae) {
+				System.out.println("You can't divide " + numerator + " by " + denominator + "\n");
+				in.nextLine();
+			}
+			catch(InputMismatchException ime) {
+				String value = in.nextLine();
+
+				if(value.charAt(0) == 'q' || value.charAt(0) == 'Q') {
+					quit = true;
+				}
+				else {
+					System.out.println("You entered bad data.");
+					System.out.println("Try again.\n");
+				}
+			}
+		}
+	}
+
+}
