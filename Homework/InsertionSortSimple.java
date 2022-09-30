673e609300c185858ba6bc84d8ff61d8422ed548
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-09-30 13:50:24 -0400

    Create InsertionSortSimple.java

---
+++ Homework/InsertionSortSimple.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main
+{
+    public static void insertionSort(double array[]) {
+        for(int i = 0; i < array.length; i++) {
+            double temp = array[i];
+            int j = i - 1;
+            while(j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }
+    
+	public static void main(String[] args) {
+		double array[] = {2.2, 2.1, 1.7, 1.2, 5.5, 0.9};
+		insertionSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+}
