969c4194c09d5e726d6c38ff34eac1bc6a7d4c57
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-10-03 13:36:07 -0400

    Create BubbleSortSimple.java

---
+++ Homework/BubbleSortSimple.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class BubbleSortSimple {
+	public static void bubbleSort(double array[]) {
+		boolean sorted = false;
+		for(int i = array.length - 1; i > 0 && !sorted; i--) {
+			sorted = true;
+			for(int j = 0; j < i; j++) {
+				double temp = array[j];
+				if(temp > array[j + 1]) {
+					sorted = false;
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int array[] = {23, 1, 64, 42, 99, 25};
+		System.out.println(Arrays.toString(array));
+		bubbleSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+}
