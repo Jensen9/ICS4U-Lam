b25579ed50096d14ec8c6061adccc43bda171629
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-10-10 23:40:15 -0400

    Create PrimeNumber.java

---
+++ Homework/PrimeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+   public static boolean prime (int n, int d) {
+      if (d == 1) {
+         return true;
+      } else if (n % d == 0) {
+         System.out.println("Divisible by " + d);
+         return false;
+      } else {
+         return prime(n, (d - 1));
+      }
+   }
+   
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      
+      System.out.print("Enter a number: ");
+      int n = in.nextInt();
+      int d = n - 1;
+      
+      boolean prime = prime(n, d);
+      if (prime) {
+         System.out.println(n + " is a prime number");
+      } else {
+         System.out.println(n + " is not a prime number");
+      }
+   }
+}
