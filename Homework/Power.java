8c0b141100b96984ebe0fa80bc23d337295b2324
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-10-11 13:32:04 -0400

    Update Power.java

--- Homework/Power.java
+++ Homework/Power.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
-
 public class Power {
-   public static int power (int x, int n, int sum) {
-      if (n == 0) {
-         return 1;
-      } else if (n == 1) {
-         return sum;
-      } else {
-         return power(x, (n - 1), (sum * x));
-      }  
-   }
-   
-   public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
+	public static int power(int x, int n) {
+		if(n == 0) {
+			return 1;
+		}
+		else {
+			return x * power(x, n - 1);
+		}
+	}
 
-      System.out.print("Enter a number: ");
-      int x = in.nextInt();
-      System.out.print("Enter the power: ");
-      int n = in.nextInt();
-      
-      int result = power(x, n, x);
-      System.out.println(x + " to the power of " + n + " is " + result);
-   }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int x = in.nextInt();
+		System.out.print("Enter the power: ");
+		int n = in.nextInt();
+
+		int result = power(x, n);
+		System.out.println(x + " to the power of " + n + " is " + result);
+	}
 }
