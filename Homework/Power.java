483c10dfa5ea90042fc0a387ec21e16736556961
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-10-10 23:42:26 -0400

    Create Power.java

---
+++ Homework/Power.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Power {
+   public static int power (int x, int n, int sum) {
+      if (n == 0) {
+         return 1;
+      } else if (n == 1) {
+         return sum;
+      } else {
+         return power(x, (n - 1), (sum * x));
+      }  
+   }
+   
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      System.out.println("Please provide a number");
+      int x = in.nextInt();
+      System.out.println("Please provide a power");
+      int n = in.nextInt();
+      
+      int result = power(x, n, x);
+      System.out.println(x + " to the power of " + n + " is " + result);
+   }
+}
