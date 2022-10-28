05937cf352ad07083fe72b02da9ab2960d9ad9a1
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-10-28 15:15:06 -0400

    Update FractionRunner.java

--- Homework/FractionRunner.java
+++ Homework/FractionRunner.java
@@ -1,27 +1,16 @@
 public class FractionRunner {
 
 	public static void main(String[] args) {
-		Fraction f1 = new Fraction();
-		Fraction f2 = new Fraction();
+		Fraction f1 = new Fraction(3, 5);
+		Fraction f2 = new Fraction(7, -9);
 		Fraction f3;
-		Fraction f4 = new Fraction();
+		Fraction f4 = new Fraction(8.137);
 		
-		f1.num = 3;
-		f1.den = 5;
-		
-		f2.num = 7;
-		f2.den = -9;
+		System.out.println(f4);
 		
 		f3 = f1.larger(f2);
 		
 		f2.larger(f1);
-		
-		f4.num = 16;
-		f4.den = 24;
-		
-		f4.reduce();
-		
-		System.out.println(f4.num + "/" + f4.den);
 		
 		//	System.out.println(f1.size());
 		//	System.out.println(f2.size());
