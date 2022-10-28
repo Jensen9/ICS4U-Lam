5346b8b8db30d009c9803b79cb36e18d6cff7fbd
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-10-28 15:14:52 -0400

    Update Fraction.java

--- Homework/Fraction.java
+++ Homework/Fraction.java
@@ -1,10 +1,64 @@
 public class Fraction {
-	int num;
-	int den;
+	private int num;
+	private int den;
 
+	public Fraction() { // Constructors
+		num = 0;
+		den = 1;
+	}
+	
+	public Fraction(int numerator, int denominator) { 
+		num = numerator;
+		den = denominator;
+	}
+	
+	public Fraction(Fraction fraction) {
+		num = fraction.num;
+		den = fraction.den;
+	}
+	
+	public Fraction(double number) {
+		if(number % 1 == 0) {
+			num = (int) number;
+			den = 1;
+		}
+		else { 
+			int denominator = 1;
+			while(number % 1 != 0) {
+				denominator *= 10;
+				number *= 10;
+			}
+			num = (int) number;
+			den = denominator;
+		}
+	}
+	
+	public int getNumerator() { // Accessors
+		return num;
+	}
+	
+	public int getDenominator() {
+		return den;
+	}
+
+	public void setNumerator(int numerator) {
+		this.num = numerator;
+	}
+
+	public void setDenominator(int denominator) { // Mutators
+		if(num == 0) {
+			this.den = 1;
+		}
+		else {
+			this.den = denominator;
+		}
+	}
+	
+	// Methods
 	public double size() {
 		return Math.abs((double)num / den);
 	}
+	
 	public Fraction larger (Fraction other) {
 		if (size() >= other.size()) {
 			return this;
@@ -12,6 +66,7 @@ public class Fraction {
 			return other;
 		}
 	}
+	
 	public Fraction times(Fraction other) {
 		Fraction product = new Fraction();
 		
@@ -20,6 +75,7 @@ public class Fraction {
 		product.reduce();
 		return product;
 	}
+	
 	public Fraction plus (Fraction other) {
 		Fraction sum = new Fraction();
 		
@@ -28,17 +84,20 @@ public class Fraction {
 		sum.reduce();
 		return sum;
 	}
+	
 	public void timesEquals(Fraction p) {
-		num = num * p.num;
-		den = den * p.num;
+		num *= p.num;
+		den *= p.num;
 		this.reduce();
 	}
+	
 	public void reduce() {
 		int div = gcd(num, den);
 		num /= div;
 		den /= div;
 	}
-	public static int gcd(int x, int y) {
+	
+	private static int gcd(int x, int y) {
 		if(x == y) {
 			return x;
 		}
@@ -48,5 +107,17 @@ public class Fraction {
 		else {
 			return gcd(y, x);
 		}
+	}
+	
+	public boolean equals(Fraction other) {
+		return size() == other.size();
+	}
+	
+	public double compareTo(Fraction other) {
+		return size() - other.size();
+	}
+	
+	public String toString() {
+		return num  + "/" + den;
 	}
 }
