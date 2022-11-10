659d33a562995669c1314a67ab01822ef6fb92da
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-11-10 15:06:43 -0500

    Update SchoolBag.java

--- Homework/Objects Containing Objects 2/SchoolBag.java
+++ Homework/Objects Containing Objects 2/SchoolBag.java
@@ -5,12 +5,12 @@ public class SchoolBag {
 	private static int EMPTY_BAG_WEIGHT;
 	
 	// Constructors
-	public SchoolBag(Binder one, Binder two) {
+	public SchoolBag(Binder one, Binder two) { // Given two binder objects
 		binderOne = one;
 		binderTwo = two;
 	}
 	
-	public SchoolBag(String label1, String colour1, int sheets1, String label2, String colour2, int sheets2) {
+	public SchoolBag(String label1, String colour1, int sheets1, String label2, String colour2, int sheets2) { // Given binder paramaters
 		binderOne = new Binder(label1, colour1, sheets1);
 		binderTwo = new Binder(label2, colour2, sheets2);
 	}
@@ -21,7 +21,7 @@ public class SchoolBag {
 	}
 	
 	// Methods
-	public void add(int index, int sheets) {
+	public void add(int index, int sheets) { // Adding sheets based on index
 		if(index == 1) {
 			binderOne.add(sheets);
 		}
@@ -30,7 +30,7 @@ public class SchoolBag {
 		}
 	}
 	
-	public void remove(int index, int sheets) {
+	public void remove(int index, int sheets) { // Removing sheets based on index
 		if(index == 1) {
 			binderOne.add(0 - sheets);
 		}
@@ -39,15 +39,15 @@ public class SchoolBag {
 		}
 	}
 	
-	public int average() {
+	public int average() { // Calculating average weight of two binders
 		return (binderOne.getWeight() + binderTwo.getWeight()) / 2;
 	}
 	
-	public int weight() {
+	public int weight() { // Calculating total weight
 		return binderOne.getWeight() + binderTwo.getWeight() + EMPTY_BAG_WEIGHT;
 	}
 	
-	public String toString() {
+	public String toString() { // Printing schoolbag information
 		return "Style: " + style + "\nBinder #1: \n" + binderOne
 				+ "\nBinder #2: \n" + binderTwo;
 	}
