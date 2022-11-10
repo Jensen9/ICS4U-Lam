365e96b5e7719d7ef5ba09745229c56947387c8b
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-11-09 23:42:22 -0500

    Create Computer.java

---
+++ Homework/Array Of Objects/Computer.java
@@ -0,0 +1,112 @@
+public class Computer {
+	private String serial;
+	private String manufacturer;
+	private int yearMade;
+	private int yearPurchased;
+	private double processorSpeed;
+	private int ram;
+	private int warranty;
+	
+	// Constructor
+	public Computer(String serial, String manufacturer, int yearMade, int yearPurchased, double processorSpeed, int ram, int warranty) {
+		this.serial = serial;
+		this.manufacturer = manufacturer;
+		this.yearMade = yearMade;
+		this.yearPurchased = yearPurchased;
+		this.processorSpeed = processorSpeed;
+		this.ram = ram;
+		this.warranty = warranty;
+	}
+	
+	// Accessors
+	public String getSerial() {
+		return serial;
+	}
+	
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	
+	public int getYearMade() {
+		return yearMade;
+	}
+	
+	public int getYearPurchased() {
+		return yearPurchased;
+	}
+	
+	public double getProcessorSpeed() {
+		return processorSpeed;
+	}
+	
+	public int getRam() {
+		return ram;
+	}
+	
+	public int getWarranty() {
+		return warranty;
+	}
+	
+	// Mutators
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+	
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	
+	public void setYearMade(int yearMade) {
+		this.yearMade = yearMade;
+	}
+	
+	public void setYearPurchased(int yearPurchased) {
+		this.yearPurchased = yearPurchased;
+	}
+	
+	public void setProcessorSpeed(int processorSpeed) {
+		this.processorSpeed = processorSpeed;
+	}
+	
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+	
+	public void setWarranty(int warranty) {
+		this.warranty = warranty;
+	}
+	
+	// Methods
+	public String toString() {
+		return "Serial: " + serial + "\nManufacturer: " + manufacturer  + "\nYear Made: " + yearMade
+				+ "\nYear Purchased: " + yearPurchased + "\nProcessor Speed: " + processorSpeed + "GHz\nRAM: " 
+				+ ram + "GB\nWarranty: " + warranty;
+	}
+	
+	public Computer faster(Computer other) {
+		if(this.processorSpeed > other.processorSpeed) {
+			return this;
+		}
+		else {
+			return other;
+		}
+	}
+	
+	public Computer moreRAM(Computer other) {
+		if(this.ram > other.ram) {
+			return this;
+		}
+		else {
+			return other;
+		}
+	}
+	
+	public Computer older(Computer other) {
+		if(this.yearMade < other.yearMade) {
+			return other;
+		}
+		else {
+			return this;
+		}
+	}
+}
