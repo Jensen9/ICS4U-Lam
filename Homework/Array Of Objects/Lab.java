dd68464538da1522cbbc2e44fac23dfc8c60ac98
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-11-10 15:15:51 -0500

    Update Lab.java

--- Homework/Array Of Objects/Lab.java
+++ Homework/Array Of Objects/Lab.java
@@ -1,13 +1,84 @@
 public class Lab {
-	private static int MAX_CAPACITY = 10;
-	private int computers;
+	private int maxCapacity;
+	private int numInstalled = 0;
 	Computer[] computer;
 	
 	// Constructor
 	public Lab(Computer[] list) {
-		computer = new Computer[MAX_CAPACITY];
-		for(int i = 0; i < MAX_CAPACITY; i++) {
+		computer = new Computer[list.length];
+		for(int i = 0; i < list.length; i++) {
 			computer[i] = list[i];
 		}
+		numInstalled += list.length;
+	}
+	
+	public boolean installComputer(Computer a) {
+		if(numInstalled < maxCapacity) {
+			computer[numInstalled] = new Computer(a.getSerial(), a.getManufacturer(), a.getYearMade(), a.getYearPurchased(), a.getProcessorSpeed(), a.getRam(), a.getWarranty());
+			numInstalled++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public int averageAge() {
+		int temp = 0;
+		for(int i = 0; i < computer.length; i++) {
+			temp += 2022 - computer[i].getYearMade();
+		}
+		return temp / computer.length;
+	}
+	
+	public Computer newest() {
+		Computer temp = computer[0];
+		for(int i = 1; i < computer.length; i++) {
+			if(temp.getYearMade() > computer[i].getYearMade()) {
+				temp = computer[i];
+			}
+		}
+		return temp;
+	}
+	
+	public Computer fastest() {
+		Computer temp = computer[0];
+		for(int i = 1; i < computer.length; i++) {
+			if(temp.getProcessorSpeed() < computer[i].getProcessorSpeed()) {
+				temp = computer[i];
+			}
+		}
+		return temp;
+	}
+	
+	public Computer mostRAM() {
+		Computer temp = computer[0];
+		for(int i = 1; i < computer.length; i++) {
+			if(temp.getRam() < computer[i].getRam()) {
+				temp = computer[i];
+			}
+		}
+		return temp;
+	}
+	
+	public int madeBy(String manufacturer) {
+		int made = 0;
+		for(int i = 0; i < computer.length; i++) {
+			if(computer[i].getManufacturer().equals(manufacturer)) {
+				made++;
+			}
+		}
+		return made;
+	}
+	
+	public Computer[] expiring() {
+		Computer expiring[] = new Computer[1];
+		for(int i = 0; i < computer.length; i++) {
+			if(computer[i].getWarranty() == 2023) {
+				expiring[expiring.length] = computer[i];
+			}
+		}
+		
+		return expiring;
 	}
 }
