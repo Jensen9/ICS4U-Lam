512894173e2835cdd1c12a3e2741371300e6a286
Jensen9 <91484847+Jensen9@users.noreply.github.com>
2022-11-14 00:05:12 -0500

    Create ClosetRunner.java

---
+++ Homework/Array of Objects 2/ClosetRunner.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ClosetRunner {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Max Shirts: ");
+		int max = in.nextInt();
+		in.nextLine();
+		
+		Closet closet = new Closet(max);
+		
+		try {
+			BufferedReader read = new BufferedReader(new FileReader("boyShirts.txt"));
+			
+			int numShirts = Integer.parseInt(read.readLine());
+			for(int i = 0; i < numShirts; i++) {
+				String colour = read.readLine();
+				int size = Integer.parseInt(read.readLine());
+				int yearPurchased = Integer.parseInt(read.readLine());
+				
+				closet.addShirt(colour, size, yearPurchased);
+			}
+			
+		} catch(IOException e) {
+			System.out.println("File error");
+		}
+		
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Shirt #" + (i + 1) + " colour: ");
+			String colour = in.nextLine();
+			
+			System.out.println("Shirt #" + (i + 1) + " size: ");
+			int size = in.nextInt();
+			
+			System.out.println("Shirt #" + (i + 1) + " year purchased: ");
+			int yearPurchased = in.nextInt();
+			
+			if(closet.getMaxShirt() == closet.getNumShirt()) {
+				closet.removeOldest();
+			}
+			in.nextLine();
+			closet.addShirt(colour, size, yearPurchased);
+		}
+
+		System.out.print("\nSize of shirt: ");
+		int size = in.nextInt();
+		
+		System.out.println("There are " + closet.shirtsSized(size) + " sized " + size + "\n");
+		
+		System.out.println(closet.biggest());
+	}
+}
